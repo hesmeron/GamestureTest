cf8cbbff81a232fa43fcbcb81a2fd34570ee7d41
MSI\hesmeron <kr201@interia.pl>
2020-01-09 21:27:38 +0100

    Initialize repository and add ui meshes

---
+++ Assets/UI/Button/Button3D.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button3D : MonoBehaviour
+{
+    [SerializeField]
+    public Action action;
+    public Func<int> function;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnMouseDown()
+    {
+        
+    }
+}
